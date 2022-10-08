0aa453ea914af0cd45dd2efe9a1071c4b442c8d1
WebQuant <74183847+WebQuant@users.noreply.github.com>
2022-10-08 17:55:20 +0300

    Add files via upload

---
+++ MindBoxTestTask/Lib/CalcArea.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lib
+{
+    public class CalcFiguresArea
+    {
+        public double CircleAreaCalc(int r)
+        {
+            Console.Write("Result: ");
+            return Math.Round(Math.PI * Math.Pow(r, 2), 2);
+        }
+        
+        public double TriangleAreaCalc(int a, int b, int c)
+        {
+            double p = (a + b + c) / 2d;
+            
+            Console.Write("Result: ");
+            return Math.Round(Math.Sqrt(p * (p - a) * (p - b) * (p - c)), 2);
+        }
+
+        public bool IsTriangleRectangular(int a, int b, int c)
+        {
+            // multiplication can be replaced by a square using the Math.Pow() function
+            if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (c * c + b * b == a * a)) 
+                return true;
+            return false;
+        }
+    }
+}
---
+++ MindBoxTestTask/MindBoxTestTask/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Lib;
+
+namespace MindBoxTestTask
+{
+    internal class Program
+    {
+        private static CalcFiguresArea cfa = new CalcFiguresArea();
+        
+        static void IsRectangularChoice(int a, int b, int c)
+        {
+            Console.Write("Do you want to check the triangle for squareness? (yes/y OR no/n)\n" +
+                          "> ");
+
+            string choiceRectangular = Console.ReadLine();
+            
+            switch (choiceRectangular)
+            {
+                case "":
+                case "yes":
+                case "y":
+                {
+                    Console.WriteLine(cfa.IsTriangleRectangular(a, b, c)
+                        ? "Triangle is rectangular"
+                        : "Triangle is not rectangular");
+                    break;
+                }
+                case "no":
+                case "n":
+                {
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("Choose yes/y or no/n");
+                    break;
+                }
+            }
+        }
+        
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("The area of which shape you want to calculate?\n" +
+                                  "1). The area of the circle by radius\n" +
+                                  "2). The area of the triangle by three sides\n" +
+                                  "Enter the number of the menu item and press ENTER.\n" +
+                                  "Enter 0 to exit the program.");
+                Console.Write("> ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                
+                if (choice==0) break; // exiting the program
+                
+                switch (choice)
+                {
+                    case 1:
+                    {
+                        Console.Write("Enter radius: ");
+                        int r = Convert.ToInt32(Console.ReadLine());
+                        // calculating the area of a circle by radius
+                        Console.WriteLine(cfa.CircleAreaCalc(r));
+                        
+                        Console.WriteLine("To exit the menu, press any key");
+                        Console.ReadLine();
+                        break;
+                    }
+                    case 2:
+                    {
+                        Console.Write("Enter a: ");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter b: ");
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter c: ");
+                        int c = Convert.ToInt32(Console.ReadLine());
+
+                        // optionally checking the triangle for rectangularity
+                        IsRectangularChoice(a, b, c);
+                        
+                        // calculating the area of a triangle on three sides
+                        Console.WriteLine(cfa.TriangleAreaCalc(a, b, c));
+                        
+                        Console.WriteLine("To exit the menu, press any key");
+                        Console.ReadLine();
+                        break;
+                    }
+                    default:
+                    {
+                        Console.WriteLine("Enter the correct value");
+                        
+                        Console.WriteLine("To exit the menu, press any key");
+                        Console.ReadLine();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
